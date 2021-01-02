873f029760c31d338fd1d2d8bfbb5c069c67deec
zishone <zishgarces@gmail.com>
2021-01-02 16:59:13 +0100

    Add openapi components

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,18 @@
 import './types/express';
+import { fields, filter, limit, page, skip, sort } from './openapi/components/parameters';
+import { OpenAPIV3 } from 'openapi-types';
 
 export { mquery } from './mquery';
+
+export const openapi: { components: { parameters: { [parameterName: string]: OpenAPIV3.ParameterObject } } } = {
+  components: {
+    parameters: {
+      filter,
+      fields,
+      sort,
+      page,
+      skip,
+      limit,
+    },
+  },
+};
---
+++ src/openapi/components/parameters/index.ts
@@ -0,0 +1,6 @@
+export { fields } from './fields';
+export { filter } from './filter';
+export { limit } from './limit';
+export { page } from './page';
+export { skip } from './skip';
+export { sort } from './sort';
